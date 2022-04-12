735c478180da9f9c0a8df06662d729b4d8df4136
Larissa_Gomes <88160134+LarissaGomes333@users.noreply.github.com>
2022-04-12 07:32:20 -0300

    Add files via upload

---
+++ build/precache-manifest.499653544b25a84be4bdb1ac7210e63e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "62adde145f6938ad5f8a",
+    "url": "/static/css/main.2fb652a2.chunk.css"
+  },
+  {
+    "revision": "62adde145f6938ad5f8a",
+    "url": "/static/js/main.62adde14.chunk.js"
+  },
+  {
+    "revision": "7d8a5b69bced8be18895",
+    "url": "/static/js/1.7d8a5b69.chunk.js"
+  },
+  {
+    "revision": "f99e647735c0b38cb0ab",
+    "url": "/static/js/2.f99e6477.chunk.js"
+  },
+  {
+    "revision": "43928d61158871188870",
+    "url": "/static/js/runtime~main.43928d61.js"
+  },
+  {
+    "revision": "8371f05f043c719152cacab7fd617e87",
+    "url": "/static/media/Logo.8371f05f.png"
+  },
+  {
+    "revision": "ae693a08e696ef9b1aaa2686a316f885",
+    "url": "/static/media/facebranco.ae693a08.png"
+  },
+  {
+    "revision": "a75048865651f9d5b32839f8db5b0067",
+    "url": "/static/media/appstore.a7504886.png"
+  },
+  {
+    "revision": "05d6ee25dd27f27428a67b257f91b3bf",
+    "url": "/index.html"
+  }
+];
